e2ddf6e151d2c2cb30be36e696cc74eb6df5b810
Peter <thong01677@gmail.com>
2021-03-05 15:52:06 -0500

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+  	@about_me = "my name is Peter Nguyen :V:V::"
+  	
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :friends
+  # get 'home/index'
+  root 'friends#index'
+  get 'home/about'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
